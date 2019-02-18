7dd15cfb70b38107a182bd878f0644263b010419
Matthew Via <matt.via@omnivore.io>
2019-02-18 13:31:59 -0500

    Reverse logic for readability

--- src/tfi.c
+++ src/tfi.c
@@ -12,20 +12,22 @@
 
 static void handle_fuel_pump() {
   static timeval_t last_valid = 0;
+
   /* If engine is turning, keep pump on */
-  if (config.decoder.state != DECODER_NOSYNC) {
+  if ((config.decoder.state == DECODER_RPM) ||
+      (config.decoder.state == DECODER_SYNC)) {
     last_valid = current_time();
     set_gpio(config.fueling.fuel_pump_pin, 1);
     return;
   }
 
   /* Allow 4 seconds of fueling */
-  if (time_diff(current_time(), last_valid) > time_from_us(4000000)) {
+  if (time_diff(current_time(), last_valid) < time_from_us(4000000)) {
+    set_gpio(config.fueling.fuel_pump_pin, 1);
+  } else {
     set_gpio(config.fueling.fuel_pump_pin, 0);
     /* Keep last valid 5 seconds behind to prevent rollover bugs */
     last_valid = time_diff(current_time(), time_from_us(5000000));
-  } else {
-    set_gpio(config.fueling.fuel_pump_pin, 1);
   }
 }
 
@@ -35,12 +37,6 @@ static void handle_boost_control() {
 static void handle_idle_control() {
 }
 
-static void low_priority_cycle() {
-    console_process();
-    handle_fuel_pump();
-    handle_boost_control();
-    handle_idle_control();
-}
 
 int main() {
   platform_load_config();
@@ -53,7 +49,11 @@ int main() {
 
   while (1) {                   
     stats_increment_counter(STATS_MAINLOOP_RATE);
-    low_priority_cycle();
+
+    console_process();
+    handle_fuel_pump();
+    handle_boost_control();
+    handle_idle_control();
   }
 
   return 0;
