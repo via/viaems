5d512901eaba3c7e04a0452661ef2f6073ca691d
Matthew Via <matt.via@omnivore.io>
2019-03-09 21:22:21 -0500

    Replace some non-emergency asserts with returns

--- src/scheduler.c
+++ src/scheduler.c
@@ -299,8 +299,9 @@ schedule_ignition_event(struct output_event *ev,
   timeval_t start_time;
   int firing_angle;
 
-  assert(d->rpm > 0);
-  assert(config.decoder.valid);
+  if (!d->rpm || !config.decoder.valid) {
+    return 0;
+  }
 
   firing_angle = clamp_angle(ev->angle - advance - 
       d->last_trigger_angle + d->offset, 720);
@@ -352,8 +353,9 @@ schedule_fuel_event(struct output_event *ev,
   timeval_t start_time;
   int firing_angle;
 
-  assert(d->rpm > 0);
-  assert(config.decoder.valid);
+  if (!d->rpm || !config.decoder.valid) {
+    return 0;
+  }
 
   firing_angle = clamp_angle(ev->angle - d->last_trigger_angle + 
     d->offset, 720);
@@ -391,8 +393,9 @@ schedule_adc_event(struct output_event *ev, struct decoder *d) {
   int firing_angle;
   timeval_t collect_time;
 
-  assert(d->rpm > 0);
-  assert(config.decoder.valid);
+  if (!d->rpm || !config.decoder.valid) {
+    return 0;
+  }
 
   firing_angle = clamp_angle(ev->angle - d->last_trigger_angle + 
     d->offset, 720);
