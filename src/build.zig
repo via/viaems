2c125c1b3aa39721f9d077b4c0ed7fdfac359d75
Matthew Via <matt.via@omnivore.io>
2019-07-08 08:59:37 -0400

    more

--- src/build.zig
+++ src/build.zig
@@ -1,18 +1,11 @@
-const Builder = @import("std").build.Builder;
+const std = @import("std");
 const builtin = @import("builtin");
 
-pub fn build(b: *Builder) void {
+pub fn build(b: *std.build.Builder) void {
     const mode = b.standardReleaseOptions();
     const exe = b.addExecutable("tfi", "main.zig");
     exe.setBuildMode(mode);
 
-    exe.setTarget(builtin.Arch{ .thumb = builtin.Arch.Arm32.v7em }, builtin.Os.freestanding, builtin.Abi.eabihf);
-
-    const args = [_][]const u8{
-        "-O3",              "-DSTM32F4",            "-DTICKRATE=4000000",
-        "-mfloat-abi=hard", "-mfpu=fpv4-sp-d16",    "-mthumb",
-        "-mcpu=cortex-m4",  "-fno-strict-aliasing", "-flto",
-    };
     const c_sources = [_][]const u8{
         "decoder.c",
         "util.c",
@@ -25,13 +18,46 @@ pub fn build(b: *Builder) void {
         "stats.c",
         "tasks.c",
     };
-    exe.setLinkerScriptPath("platforms/stm32f4-discovery.ld");
-    exe.addCSourceFile("platforms/stm32f4-discovery.c", args);
-    for (c_sources) |source| {
-        exe.addCSourceFile(source, args);
-    }
+
+    //    platform_stm32f407(exe, c_sources);
+    platform_hosted(exe, c_sources);
 
     exe.addIncludeDir(".");
+    exe.setOutputDir("output");
+
+    b.default_step.dependOn(&exe.step);
+    b.installArtifact(exe);
+}
+
+fn platform_hosted(exe: *std.build.LibExeObjStep, c_sources: []const []const u8) void {
+    const c_args = [_][]const u8{"-DTICKRATE=1000000",
+    "-DSUPPORTS_POSIX_TIMERS", "-D_POSIX_C_SOURCE=199309L", "-D_GNU_SOURCE"};
+
+    exe.setTarget(builtin.Arch.x86_64, builtin.Os.linux, builtin.Abi.gnu);
+    exe.addCSourceFile("platforms/hosted.c", c_args);
+    for (c_sources) |source| {
+        exe.addCSourceFile(source, c_args);
+    }
+
+    exe.linkSystemLibrary("c");
+    exe.linkSystemLibrary("m");
+    exe.linkSystemLibrary("rt");
+}
+
+fn platform_stm32f407(exe: *std.build.LibExeObjStep, c_sources: []const []const u8) void {
+    const c_args = [_][]const u8{
+        "-O3",              "-DSTM32F4",            "-DTICKRATE=4000000",
+        "-mfloat-abi=hard", "-mfpu=fpv4-sp-d16",    "-mthumb",
+        "-mcpu=cortex-m4",  "-fno-strict-aliasing", "-flto",
+    };
+    exe.setTarget(builtin.Arch{ .thumb = builtin.Arch.Arm32.v7em }, builtin.Os.freestanding, builtin.Abi.eabihf);
+    exe.setLinkerScriptPath("platforms/stm32f4-discovery.ld");
+
+    exe.addCSourceFile("platforms/stm32f4-discovery.c", c_args);
+    for (c_sources) |source| {
+        exe.addCSourceFile(source, c_args);
+    }
+
     exe.addIncludeDir("../libopencm3/include");
     exe.addIncludeDir("/usr/lib/arm-none-eabi/include");
     exe.addLibPath("../libopencm3/lib");
@@ -40,13 +66,4 @@ pub fn build(b: *Builder) void {
     exe.addObjectFile("/usr/lib/arm-none-eabi/lib/thumb/v7e-m/libm.a");
     exe.addObjectFile("/usr/lib/arm-none-eabi/lib/thumb/v7e-m/libnosys.a");
     exe.addObjectFile("/usr/lib/gcc/arm-none-eabi/7.3.1/thumb/v7e-m/fpv4-sp/hard/libgcc.a");
-    exe.setOutputDir("output");
-
-    const run_cmd = exe.run();
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    b.default_step.dependOn(&exe.step);
-    b.installArtifact(exe);
 }
