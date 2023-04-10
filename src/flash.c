60f65a19ce953e077ac7781cdbae880bd6f796e4
Matthew Via <via@matthewvia.info>
2023-04-10 17:24:14 -0400

    Enable ability to write/erase

--- src/flash.c
+++ src/flash.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <string.h>
 
+#include "platform.h"
 #include "flash.h"
 
 static uint8_t spi_tx_buffer[520];
@@ -58,6 +59,23 @@ bool flash_read(struct flash *f,  uint8_t *dest, uint32_t address, size_t length
   return true;
 }
 
+bool flash_is_busy(struct flash *f) {
+  const uint8_t read_sr1_cmd[] = {0x5, 0x00};
+  uint8_t read_sr1_resp[2];
+
+  flash_spi_transaction(read_sr1_cmd, read_sr1_resp, sizeof(read_sr1_cmd));
+  return (read_sr1_resp[1] & 0x1) != 0; 
+}
+
+bool flash_enable_write(struct flash *f) {
+  const uint8_t write_enable_cmd[] = {0x6};
+  uint8_t write_enable_resp[1];
+
+  flash_spi_transaction(write_enable_cmd, write_enable_resp, sizeof(write_enable_cmd));
+  while (flash_is_busy(f));
+  return true;
+}
+
 bool flash_erase_sector(struct flash *f, uint32_t address) {
   if (f->valid) { 
     return false;
@@ -70,13 +88,49 @@ bool flash_erase_sector(struct flash *f, uint32_t address) {
     return false;
   }
 
+  flash_enable_write(f);
+
   uint8_t cmd[] = {0x20, 
                    (address & 0xff0000) >> 16,
                    (address & 0xff00) >> 8,
                    (address & 0xff),
                    };
 
+  uint8_t resp[4];
+
+  flash_spi_transaction(cmd, resp, sizeof(cmd));
+
+  while(flash_is_busy(f));
+  return true;
 }
 
 bool flash_write(struct flash *f, const uint8_t *src, uint32_t address, size_t length) {
+  if (!f->valid) { 
+    return false;
+  }
+  uint32_t start_page = address / f->page_size;
+  uint32_t end_page = (address + length - 1) / f->page_size;
+
+  if (start_page != end_page) {
+    /* Full written range must be in single page */
+    return false;
+  }
+  if ((address + length) >= f->capacity_bytes) {
+    return false;
+  }
+
+  flash_enable_write(f);
+
+  uint8_t cmd[] = {0x02, 
+                   (address & 0xff0000) >> 16,
+                   (address & 0xff00) >> 8,
+                   (address & 0xff),
+                   };
+  memcpy(spi_tx_buffer, cmd, sizeof(cmd));
+  memcpy(spi_tx_buffer + sizeof(cmd), src, length);
+  flash_spi_transaction(spi_tx_buffer, spi_rx_buffer, sizeof(cmd) + length);
+
+  while (flash_is_busy(f));
+  return true;
+
 }
